b5e0b1ded6b392dacd392f4915113f58eb42cfdf
dheeraj932 <dheerajkumar.1379@gmail.com>
2025-12-12 01:29:29 -0500

    welcome page is created

---
+++ finsight-app/client/src/pages/WelcomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const WelcomePage = () => {
+  return (
+    <section className="welcome-page">
+      {/* Later: paste content from welcome.html */}
+      <div className="container text-center py-5">
+        <h1>Welcome to Finsight</h1>
+        <p className="text-muted">
+          This will become your 3-step onboarding from welcome.html.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default WelcomePage;
